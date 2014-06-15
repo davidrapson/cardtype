add54229148ea846f18fddde7492d38557869f62
David Rapson <david@davidrapson.co.uk>
2014-06-15 19:50:47 +0100

    Add coverage reporting

--- karma.conf.js
+++ karma.conf.js
@@ -29,14 +29,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'cardtype.js': ['coverage']
+    },
 
+
+    // optionally, configure the reporter
+    coverageReporter: {
+        type : 'html',
+        dir : 'coverage/'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
@@ -64,5 +71,6 @@ module.exports = function(config) {
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
+
   });
 };
